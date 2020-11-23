b6ee799b1cc628ae7e12672ffb2da6751cac1ff5
Moses Ramirez <mosesgramirez@gmail.com>
2020-11-23 15:37:31 -0600

    Create header, about me, and sample project sections with CSS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Ensures the header takes up the full height of the window
+$(document).ready(function() {
+    $('.header').height($(window).height());
+
+})
+
+$(".navbar a").click(function() {
+    $("body,html").animate({
+        scrollTop: $("#" + $(this).data('value')).offset().top
+    }, 1000)
+
+})
