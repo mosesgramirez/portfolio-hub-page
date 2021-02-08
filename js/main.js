0d546c5d3d884137f9230cb84a4a90b0130778d7
Moses Ramirez <mosesgramirez@gmail.com>
2021-02-07 23:44:23 -0600

    Update images, add footer info

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 // Ensures the header takes up the full height of the window
 $(document).ready(function() {
     $('.header').height($(window).height());
-
 })
 
-$(".navbar a").click(function() {
+$("a").click(function() {
     $("body,html").animate({
         scrollTop: $("#" + $(this).data('value')).offset().top
     }, 1000)
-
 })
